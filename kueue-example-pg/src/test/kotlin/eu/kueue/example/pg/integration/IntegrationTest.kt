9dd052726fcd3777421a6f4a8ec9cacf1b5c8875
Chris de Kok <chris.de.kok@gmail.com>
2025-03-20 18:09:15 +0100

    Batch process messages

--- kueue-example-pg/src/test/kotlin/eu/kueue/example/pg/integration/IntegrationTest.kt
+++ kueue-example-pg/src/test/kotlin/eu/kueue/example/pg/integration/IntegrationTest.kt
@@ -2,6 +2,7 @@ package eu.kueue.example.pg.integration
 
 import eu.kueue.*
 import eu.kueue.example.pg.SerializerType
+import eu.kueue.example.pg.message.RecordCreated
 import eu.kueue.example.pg.message.RecordUpdated
 import eu.kueue.example.pg.serializer
 import eu.kueue.pg.vertx.PgConsumer
@@ -9,7 +10,9 @@ import eu.kueue.pg.vertx.PgProducer
 import io.vertx.pgclient.PgBuilder
 import io.vertx.pgclient.PgConnectOptions
 import io.vertx.sqlclient.PoolOptions
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import mu.KotlinLogging
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -19,7 +22,9 @@ import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
-import java.lang.System.getenv
+import kotlin.concurrent.timer
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 private const val PG_USER = "test"
 private const val PG_PASS = "t3st"
@@ -87,11 +92,6 @@ class IntegrationTest {
         serializer = serializer,
     )
 
-    private val listener = CountListener(
-        consumer = consumer,
-        stopOnCount = messageCount,
-    )
-
     @Test
     @Order(1)
     fun `test producer send`() = runBlocking {
@@ -115,13 +115,52 @@ class IntegrationTest {
     @Test
     @Order(2)
     fun `test consumer receive`() = runBlocking {
+        val listener = CountListener(
+            consumer = consumer,
+            stopOnCount = messageCount,
+        )
         consumer.subscribe<Message>(
             topic = TOPIC,
             batchSize = 8,
             listeners = listOf(listener)
         )
-        consumer.start()
-        assertEquals(messageCount, listener.totalReceived)
+        consumeWithTimeout(consumer) {
+            assertEquals(messageCount, listener.totalReceived)
+        }
+    }
+
+    @Test
+    @Order(3)
+    fun `test consumer receive bulk`() = runBlocking {
+        val sendMessageCount = 100
+        val producer = PgProducer(
+            client = pool,
+            serializer = serializer,
+        )
+
+        assertDoesNotThrow {
+            repeat(sendMessageCount) {
+                val message = RecordCreated(
+                    id = it,
+                    title = "test $sendMessageCount"
+                )
+                producer.send(TOPIC, message)
+            }
+        }
+
+        val listener = CountListener(
+            consumer = consumer,
+            stopOnCount = messageCount,
+        )
+        consumer.subscribe<Message>(
+            topic = TOPIC,
+            batchSize = 10,
+            listeners = listOf(listener)
+        )
+        consumeWithTimeout(consumer) {
+            assertEquals(sendMessageCount, listener.totalReceived)
+            assertEquals(10, listener.listenerCalled)
+        }
     }
 
     class CountListener(
@@ -129,10 +168,22 @@ class IntegrationTest {
         private val stopOnCount: Int,
     ) : EventListener {
         var totalReceived = 0
+        var listenerCalled = 0
 
         @EventHandler
         suspend fun on(event: RecordUpdated) {
             totalReceived++
+            listenerCalled++
+            logger.info { "received $event" }
+            if (totalReceived == stopOnCount) {
+                consumer.stop()
+            }
+        }
+
+        @EventHandler
+        suspend fun on(event: List<RecordCreated>) {
+            totalReceived += event.size
+            listenerCalled++
             logger.info { "received $event" }
             if (totalReceived == stopOnCount) {
                 consumer.stop()
@@ -140,3 +191,18 @@ class IntegrationTest {
         }
     }
 }
+
+suspend fun consumeWithTimeout(
+    consumer: Consumer,
+    timeout: Duration = 15.seconds,
+    block: suspend () -> Unit
+) =
+    try {
+        withTimeout(timeout) {
+            consumer.start()
+            block()
+        }
+    } catch (e: TimeoutCancellationException) {
+        consumer.stop()
+        throw e
+    }
